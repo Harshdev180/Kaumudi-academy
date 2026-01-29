b53966128b819f0cdc7c3c76e6a50f0701932479
Abhishek saini <abhisheksaini4952@gmail.com>
2026-01-29 20:34:54 +0530

    some changes in dashboard

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,49 +1,62 @@
-import { useState } from "react";
 import "./App.css";
-// import Sidebar, { SidebarItem } from "./pages/AdminDashboard/Sidebar";
-import { BrowserRouter as Navigate, Route, Routes } from "react-router-dom";
+
+// React Router
+import { Route, Routes } from "react-router-dom";
+
+// Pages
 import Dashboard from "./pages/AdminDashboard/Dashboard";
 import AdminLayout from "./pages/AdminDashboard/AdminLayout";
 import LeadManagement from "./pages/AdminDashboard/LeadManagement";
 import AdminLogin from "./pages/AdminDashboard/AdminLogin";
 import CourseManagement from "./pages/AdminDashboard/CourseManagement";
+import AllCoursesPage from "./pages/CourseListing";
+import CourseDetail from "./pages/CourseDetail";
+import Home from "./pages/Home";
 
+// Auth
+import Signup from "./component/Auth/signup";
+import Signin from "./component/Auth/login";
 
 function App() {
   return (
-    <>
+    <Routes>
+      {/* ---------------- Public Routes ---------------- */}
+      <Route path="/" element={<Home />} />
+      <Route path="/allcourses" element={<AllCoursesPage />} />
+      <Route path="/coursedetail" element={<CourseDetail />} />
+      <Route path="/signup" element={<Signup />} />
+      <Route path="/login" element={<Signin />} />
 
-      <Routes>
-        {/* <Route path='/' element={<Navigate to='/dashboard' />} /> */}
-
-        <Route path='/admin/dashboard' element={
+      {/* ---------------- Admin Routes ---------------- */}
+      <Route
+        path="/admin/dashboard"
+        element={
           <AdminLayout>
             <Dashboard />
           </AdminLayout>
-        } />
+        }
+      />
 
-        <Route path='/admin/lead' element={
+      <Route
+        path="/admin/lead"
+        element={
           <AdminLayout>
             <LeadManagement />
           </AdminLayout>
-        } />
-        <Route path='/admin/courses' element={
+        }
+      />
+
+      <Route
+        path="/admin/courses"
+        element={
           <AdminLayout>
             <CourseManagement />
           </AdminLayout>
-        } />
-        <Route path='/login' element={<AdminLogin />} />
-      </Routes>
+        }
+      />
 
-
-
-      {/* <Routes>
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/leads" element={<LeadManagement />} />
-        <Route path="/courses" element={<Courses />} />
-      </Routes> */}
-      {/* <LeadsPage /> */}
-    </>
+      <Route path="/admin/login" element={<AdminLogin />} />
+    </Routes>
   );
 }
 
