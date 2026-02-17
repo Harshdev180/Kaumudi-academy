0532a195b35699a3cc60eedd85a38b588a723cca
Pranjal Soni <pranjalsoni484@gmail.com>
2026-02-17 19:45:53 +0530

    Coupon API's

--- backend/src/routes/coupon.routes.js
+++ backend/src/routes/coupon.routes.js
@@ -1,5 +1,11 @@
 import express from "express";
-import { createCoupon, toggleCouponStatus } from "../controllers/coupon.controller.js";
+import {
+  createCoupon,
+  updateCoupon,
+  toggleCouponStatus,
+  getAllCoupons
+} from "../controllers/coupon.controller.js";
+
 import { authMiddleware } from "../middlewares/auth.middleware.js";
 import { roleMiddleware } from "../middlewares/role.middleware.js";
 import { validateBody } from "../middlewares/validate.middleware.js";
@@ -7,6 +13,14 @@ import { createCouponSchema } from "../validators/coupon.validator.js";
 
 const router = express.Router();
 
+/**
+ * PUBLIC
+ */
+router.get("/coupon", getAllCoupons);
+
+/**
+ * ADMIN
+ */
 router.post(
   "/coupon",
   authMiddleware,
@@ -15,8 +29,16 @@ router.post(
   createCoupon
 );
 
+router.put(
+  "/coupon/:id",
+  authMiddleware,
+  roleMiddleware("ADMIN"),
+  validateBody(createCouponSchema),
+  updateCoupon
+);
+
 router.patch(
-  "/coupon/:id/toggle",
+  "/coupon/:id/status",
   authMiddleware,
   roleMiddleware("ADMIN"),
   toggleCouponStatus
